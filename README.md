19c7695724e7d10ad7745d4f50391b8030ca6922
Tony Ward <8069555+ynotdraw@users.noreply.github.com>
2024-10-03 13:27:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Github Action Visual Regression Testing
+
+Can we build a visual regression tool using only GitHub Actions?
---
+++ stories/Button.stories.js
@@ -0,0 +1,59 @@
+import '@crowdstrike/glide-core/button.js';
+import { html, nothing } from 'lit';
+
+export default {
+  title: 'Button',
+  tags: ['autodocs'],
+  args: {
+    'slot="default"': 'Button',
+    disabled: false,
+    variant: 'primary',
+    size: 'large',
+  },
+  render: (args) =>
+    html` <glide-core-button
+      size=${args.size}
+      variant=${args.variant}
+      ?disabled=${args.disabled || nothing}
+    >
+      ${args['slot="default"']}
+    </glide-core-button>`,
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: ['primary', 'secondary', 'tertiary'],
+      table: {
+        defaultValue: {
+          summary: '"primary"',
+        },
+        type: { summary: '"primary" | "secondary" | "tertiary"' },
+      },
+    },
+    disabled: {
+      control: { type: 'boolean' },
+    },
+    size: {
+      control: { type: 'radio' },
+      options: ['large', 'small'],
+      table: {
+        defaultValue: {
+          summary: '"large"',
+        },
+        type: { summary: '"large" | "small"' },
+      },
+    },
+    'slot="default"': {
+      control: { type: 'text' },
+      table: {
+        type: { summary: 'Element | string' },
+      },
+      type: { name: 'string', required: true },
+    },
+  },
+};
+
+export const Primary = {
+  args: {
+    variant: 'primary',
+  },
+};
